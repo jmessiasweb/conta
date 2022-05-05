82b7d82803b17d3b25d2d55d325942081299817b
josue.messias <jmessiasweb>
2022-05-05 10:24:06 -0300

    criado classes e metodos

---
+++ src/test/kotlin/br/com/contabanco/controller/ContaControllerTest.kt
@@ -0,0 +1,36 @@
+package br.com.contabanco.controller
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+internal class ContaControllerTest {
+
+    @Test
+    fun buscarTodasContas() {
+    }
+
+    @Test
+    fun buscarSaldoConta() {
+    }
+
+    @Test
+    fun criarConta() {
+    }
+
+    @Test
+    fun debitarConta() {
+    }
+
+    @Test
+    fun creditarConta() {
+    }
+
+    @Test
+    fun apagarConta() {
+    }
+
+    @Test
+    fun getService() {
+    }
+}
---
+++ src/test/kotlin/br/com/contabanco/dto/ContaDTOTest.kt
@@ -0,0 +1,12 @@
+package br.com.contabanco.dto
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+internal class ContaDTOTest {
+
+    @Test
+    fun testaInsertConta() {
+    }
+}
