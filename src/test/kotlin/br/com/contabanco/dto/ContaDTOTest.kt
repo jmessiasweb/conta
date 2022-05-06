62d3e40276999fcbe4b19b4d29d0e4504474c0a9
josue.messias <jmessiasweb>
2022-05-06 15:28:50 -0300

    implementacao classes de teste contaDTO-clienteController

--- src/test/kotlin/br/com/contabanco/dto/ContaDTOTest.kt
+++ src/test/kotlin/br/com/contabanco/dto/ContaDTOTest.kt
@@ -1,12 +1,18 @@
 package br.com.contabanco.dto
 
-import org.junit.jupiter.api.Test
-
 import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
 
+@ExtendWith
 internal class ContaDTOTest {
 
     @Test
     fun testaInsertConta() {
+        val contaDTO = ContaDTO("1", "teste", 12.8F, 1);
+        val conta = contaDTO.insertConta()
+        assertNull(conta.codigoConta)
+        assertEquals(12.8F, conta.valorSaldo)
+
     }
 }
